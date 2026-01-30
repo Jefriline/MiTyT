ba15d5f61bc215e29833211263c96a66d36da2f5
Jefriline <Jefferlop2006@gmail.com>
2026-01-29 20:12:13 -0500

    feat: Add environment configuration and update server to use dynamic port

--- server/server.py
+++ server/server.py
@@ -1,10 +1,18 @@
+import os
+
 from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
+PORT = int(os.getenv("PORT", "5000"))
 
 app = Flask(__name__)
+
 
 @app.route("/", methods=["GET"])
 def status():
     return "app funcionando"
 
-if __name__ == '__main__': 
-    app.run(host="0.0.0.0", port=5000, debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=PORT, debug=True)
