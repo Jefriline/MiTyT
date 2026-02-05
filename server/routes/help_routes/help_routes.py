ee5326ba3fb6e01b35ab4e187d3641ac09d6771a
Jhonatan C <c.jhonatan2007@gmail.com>
2026-02-05 08:55:05 -0500

    feat: Creacíón de proceso de incripción

--- server/routes/help_routes/help_routes.py
+++ server/routes/help_routes/help_routes.py
@@ -6,3 +6,7 @@ help_bp = Blueprint('Help', __name__, url_prefix='/Help')
 @help_bp.route("/video", methods=["GET"])
 def help_page():
     return render_template("support_guide.html")
+
+@help_bp.route("/step-by-step-/SaberTyT-process", methods=["GET"])
+def guides_page():
+    return render_template("inscription_guide.html")
